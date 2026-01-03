b11002095f81dc48cc7917e5e31c1ae4bf54da33
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2026-01-03 11:46:38 +0200

    fix delete route from post to delete method

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,6 +11,6 @@ $routes->get('/user/id/(:num)', 'Home::get_user_by_id/$1');
 $routes->get('/user/username/(:alphanum)', 'Home::get_user_by_username/$1');
 $routes->get('/user/edit/(:num)', 'Home::update_user/$1');
 $routes->post('/user/edit/(:num)', 'Home::update_user/$1');
-$routes->get('/user/delete/(:num)', 'Home::delete_user/$1');
+$routes->delete('/user/delete/(:num)', 'Home::delete_user/$1');
 $routes->get('/create_user', 'Home::create_user');
 $routes->post('/create_user', 'Home::create_user');
